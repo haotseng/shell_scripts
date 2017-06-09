52ff338d9951fbdaf8ea20ffe43182dc79c43a1c
haotseng <ebspace@gmail.com>
2017-06-09 14:33:02 +0800

    Added openssl support

--- inst_python.sh
+++ inst_python.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #
-# This script can install Python with TclTk from source
+# This script can install Python from source with TclTk,OpenSSL support
 #
-
 set -e
+
 CURR_PATH=$(pwd)
 SRC_PATH=${CURR_PATH}/_work_temp/src
 BUILD_PATH=${CURR_PATH}/_work_temp/build
@@ -12,10 +12,14 @@ BUILD_PATH=${CURR_PATH}/_work_temp/build
 mkdir -p $SRC_PATH
 mkdir -p $BUILD_PATH
 
+# Debugging
+#rm -rf $BUILD_PATH/*
 
-#
-# Source code
-#
+#=======================
+# Get source code
+#=======================
+OPENSSL_TAR_GZ_SRC=https://www.openssl.org/source/openssl-1.0.2l.tar.gz
+OPENSSL_VER=1.0.2l
 PYTHON_TAR_GZ_SRC=https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
 PYTHON_VER=3.6.1
 TCL_TAR_GZ_SRC=https://prdownloads.sourceforge.net/tcl/tcl8.6.6-src.tar.gz
@@ -28,17 +32,28 @@ cd $SRC_PATH
 wget $PYTHON_TAR_GZ_SRC
 wget $TCL_TAR_GZ_SRC
 wget $TK_TAR_GZ_SRC
+wget $OPENSSL_TAR_GZ_SRC
 
 echo "Extract file from archive files..."
 cd $BUILD_PATH
 tar -zxvf $SRC_PATH/Python*.tgz
 tar -zxvf $SRC_PATH/tcl*.tar.gz
 tar -zxvf $SRC_PATH/tk*.tar.gz
+tar -zxvf $SRC_PATH/openssl*.tar.gz
 
+#=======================
+# Build OPEN SSL
+#=======================
+OPENSSL_INST_PATH=${HOME}/bin/openssl
+echo "Build OPENSSL..."
+cd ${BUILD_PATH}/openssl-${OPENSSL_VER}
+./config --prefix=${OPENSSL_INST_PATH}
+make
+make install
 
-#
+#=======================
 # Build TCL_TK
-#
+#=======================
 TCLTK_INST_PATH=${HOME}/bin/tcltk
 
 echo "Build TCL..."
@@ -53,23 +68,41 @@ cd ${BUILD_PATH}/tk${TK_VER}/unix
 make
 make install
 
-#
+#=======================
 # Build Python
-#
+#=======================
 PY_INST_PATH=${HOME}/bin/py${PYTHON_VER}
 TCLTK_INC_PATH=${TCLTK_INST_PATH}/include
 TCLTK_LIB_PATH=${TCLTK_INST_PATH}/lib
 TCLTK_OPT="--with-tcltk-includes='-I${TCLTK_INC_PATH}' --with-tcltk-libs='-L${TCLTK_LIB_PATH}'"
+OPENSSL_OPT="--with-ssl"
 #export LD_LIBRARY_PATH=${TCLTK_LIB_PATH}
 #export PATH=${TCLTK_INST_PATH}/bin:$PATH
 export CPPFLAGS=-I${TCLTK_INC_PATH}
 
 echo "Build Python..."
 cd ${BUILD_PATH}/Python-${PYTHON_VER}
+
+#
+# Modified Modules/Setup.dist for SSL
+#
+cat << EOF >> ./Modules/Setup.dist
+SSL=${OPENSSL_INST_PATH}
+_ssl _ssl.c \\
+       -DUSE_SSL -I\$(SSL)/include -I\$(SSL)/include/openssl \\
+       -L\$(SSL)/lib -lssl -lcrypto
+EOF
+
+#
+# Build
+#
 ./configure --prefix=${PY_INST_PATH} ${TCLTK_OPT}
 make
 make install
 
+#=======================
+# Done
+#=======================
 echo "Done"
 
 #
